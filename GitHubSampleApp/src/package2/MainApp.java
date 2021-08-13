e496aea209081185af72a2825f6a904ef59ef6af
Aimee <yutingliau@gmail.com>
2021-08-13 10:49:40 +0800

    7th 我是組員,用組員帳號

--- GitHubSampleApp/src/package2/MainApp.java
+++ GitHubSampleApp/src/package2/MainApp.java
@@ -2,7 +2,7 @@ package package2;
 
 public class MainApp {
 	public static void main(String[] args) {
-		System.out.print("2st Version1 ");//這是新的
+		System.out.print("2st Version1 ");//我是組員
 		Math cMath=new Math();
 		System.out.print(cMath.add(10, 5));//
 	}
