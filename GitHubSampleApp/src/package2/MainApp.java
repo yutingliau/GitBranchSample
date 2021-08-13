e4e51f64663048fffc20c36166158c09424aea70
Aimee <yutingliau@gmail.com>
2021-08-13 10:15:56 +0800

    6th

--- GitHubSampleApp/src/package2/MainApp.java
+++ GitHubSampleApp/src/package2/MainApp.java
@@ -2,7 +2,7 @@ package package2;
 
 public class MainApp {
 	public static void main(String[] args) {
-		System.out.print("2st Version1 ");//³o¬O¤@­Ó·sªº
+		System.out.print("2st Version1 ");//é€™æ˜¯æ–°çš„
 		Math cMath=new Math();
 		System.out.print(cMath.add(10, 5));//
 	}
