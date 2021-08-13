6c3b8c7c2c9de0f5d65cbbbafb844669fd68209d
Aimee <yutingliau@gmail.com>
2021-08-13 09:36:18 +0800

    2st

--- GitHubSampleApp/src/package2/MainApp.java
+++ GitHubSampleApp/src/package2/MainApp.java
@@ -1,5 +1,16 @@
 package package2;
 
 public class MainApp {
-lsr Version1
+	public static void main(String[] args) {
+		System.out.print("2st Version1");//這是一個新的
+		Math cMath=new Math();
+		System.out.print(cMath.add(10, 5));
+	}
 }
+class Math{
+	int add(int a,int b) {
+		return a+b;
+	}
+}
+
+
